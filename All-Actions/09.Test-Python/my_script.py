53cba8b9b62de4110f68e20dea31ac051e0688fa
abhinavchinna <abhi02111999@gmail.com>
2023-06-29 01:58:41 +0530

    updated my_script.py

--- All-Actions/09.Test-Python/my_script.py
+++ All-Actions/09.Test-Python/my_script.py
@@ -1,4 +1,6 @@
-file = open('test.txt','w')
-file.write("This is the write command")
-file.write("It allows us to write in a particular file")
-file.close()
+def write_data():
+    with open('test.txt','w') as file:
+        file.write("This is the write command\n")
+        file.write("It allows us to write in a particular file\n")
+if __name__=='__main__':
+    write_data()
